2a07d6a78f035b2789fc2c36cade21672136f5af
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-07 11:23:23 +0530

    Done increment decrement

--- src/pages/IncDec.js
+++ src/pages/IncDec.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IncDec = () => {
+
+    const [value, setValue] = useState(0)
+
+    const Increm = () => {
+        setValue(value + 1);   
+    }
+
+    const Decrem = () => {
+        if (value > 0) {
+            setValue(value - 1);
+        } else {
+            setValue(0);
+        }
+    }
 
     return (
         <>
         <div className="main_div">
             <div className="center_div">
-                <h1> 0 </h1>
+                <h1> {value} </h1>
                 <div className="btn_div">
-                    <button>Increm</button>
-                    <button>Decrem</button>
+                    <button onClick={Increm}>Increm</button>
+                    <button onClick={Decrem}>Decrem</button>
                 </div>
             </div>
         </div>
